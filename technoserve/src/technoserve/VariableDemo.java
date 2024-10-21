06f6ef4e112df6436d36d942e42e373fb98154e5
saleha anjum <saleha anjum@LAPTOP-KJ6U2FPP>
2024-10-21 16:41:53 +0530

    first commit

---
+++ technoserve/src/technoserve/VariableDemo.java
@@ -0,0 +1,20 @@
+package technoserve;
+
+public class VariableDemo {
+	int id =10;//inst var
+	void print()
+	{
+		String msg="hello"; //loc var
+		System.out.println(msg);/
+	}
+	static String message="Hello Students";//stat var
+	
+	
+	public static void main(String[] args) {
+		VariableDemo obj= new VariableDemo();
+		System.out.println(obj.id);
+		obj.print();
+		System.out.println(message);
+	}
+
+}
