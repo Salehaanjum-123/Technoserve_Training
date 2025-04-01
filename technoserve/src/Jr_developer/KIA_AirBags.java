e4db76a2e2b10ce156892ecb759bf21b38ececcb
saleha anjum <saleha anjum@LAPTOP-KJ6U2FPP>
2025-04-01 17:51:13 +0530

    first commit

--- technoserve/src/Jr_developer/KIA_AirBags.java
+++ technoserve/src/Jr_developer/KIA_AirBags.java
@@ -1,15 +1,16 @@
 package Jr_developer;
 
 public class KIA_AirBags {
+	
+	public void disp_AirBags()
 
-		void disp_AirBags()
-		{
-			System.out.println("All KIA have airbags");
-		}
-		public static void main(String[] args)
-		 {
-			 KIA_Speed_limit obj=new KIA_Speed_limit();
-			 obj.disp_Speed_limit();
-		 }
-
+	{
+		System.out.println("All KIA have airbags ");
+	}
+	 
+	public static void main(String[] args)
+	 {
+		 KIA_SpeedLimit obj=new KIA_SpeedLimit();
+		 obj.disp_Speed_limit();
+	 }
 }
--- technoserve/src/Jr_developer/KIA_SpeedLimit.java
+++ technoserve/src/Jr_developer/KIA_SpeedLimit.java
@@ -2,14 +2,14 @@ package Jr_developer;
 
 public class KIA_SpeedLimit {
 
-void disp_Speed_limit()
-{
-	System.out.println("All KIA have speed limit");
-	}
-     public static void main(String[] args)
-{
-	 KIA_Speed_limit obj=new KIA_Speed_limit();
-	 obj.disp_Speed_limit();
-	 }
+	 public void disp_Speed_limit()
+		{
+			System.out.println("All KIA have speedlimit ");
+		}
+	 public static void main(String[] args)
+		 {
+			 KIA_SpeedLimit obj=new KIA_SpeedLimit();
+			 obj.disp_Speed_limit();
+		 }
 
-}
+	}
